6755f254f51f616daa1a0cffc10e807c0ac7b943
verinalaura <166783472+verinalaura@users.noreply.github.com>
2024-04-17 04:11:49 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>International Spoty Songs</b></h3>
+					<p>Jakarta, Indonesia</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287720040518</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> internationalspotysongs@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Playlist</a></p>
+					<p><a href=""  style="color: #000">About Us</a></p>
+					<p><a href=""  style="color: #000">Contact Us</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #ff8680; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Ahmad Rafi Akbar Putra Hamzah</span>
+		</div>
+	</footer>
+
+</body>
+</html>
